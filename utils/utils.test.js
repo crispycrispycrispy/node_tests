b4b806646d44ba4fd2daa0735d99099c8cf53d9c
Gavin <gavinchrisdsilva@gmail.com>
2018-08-06 02:03:24 +0530

    Added assertion library and auto restart scripts

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,13 +1,38 @@
+const expect = require('expect');
 const utils = require('./utils');
+
 it('should add two numbers', () => {
     var res = utils.add(10, 20);
-    if(res != 30)
-        throw new Error(`Value expected 30 got ${res}`)
+    expect(res).toBe(30).toBeA('number');
+
+    // if(res != 30)
+    //     throw new Error(`Value expected 30 got ${res}`)
 });
 
 it('should square a number', () => {
-    var res = utils.square(3);
-    if(res != 4){
-        throw new Error('Value expecting 4 got' + res);
-    }
+    var res = utils.square(2);
+    expect(res).toBe(4).toBeA('number');
+    // if(res != 4){
+    //     throw new Error('Value expecting 4 got' + res)
+    // }
 });
+
+it('should verify first and last names are set', () => {
+    var res = utils.setName({
+        location: "Mum",
+        age: 22 
+    },"Gavin Dsilva");
+    expect(res).toInclude({
+        firstName: "Gavin",
+        lastName: "Dsilva"
+    });
+});
+
+//should verify firstName and lastNames are set
+//check if it includes first
+
+// it('should expec some values', () => {
+//     //expect(12).toNotBe(12);
+//     // expect({name: 'Andres'}).toEqual({name: 'Andres'});
+//     expect([2, 3, 4]).toInclude(2);
+// });
